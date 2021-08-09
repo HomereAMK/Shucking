8358f0e3d940f0519af1874e5e0092ff7ddb3f4d
Homère J. Alves Monteiro <hmon@aqua.dtu.dk>
2021-08-09 16:45:27 +0200

    parrallelel

--- 00_scripts/06_aDepth.sh
+++ 00_scripts/06_aDepth.sh
@@ -2,11 +2,11 @@
 #PBS -d /home/projects/dp_00007/people/hmon/Shucking
 #PBS -W group_list=dp_00007 -A dp_00007
 #PBS -N bam_depth
-#PBS -e 98_log_files/Covstat/covstat.err
-#PBS -o 98_log_files/Covstat/covstat.out
-#PBS -l nodes=2:ppn=10:thinnode
+#PBS -e 98_log_files/Covstat/__BASE__depth.err
+#PBS -o 98_log_files/Covstat/__BASE__depth.out
+#PBS -l nodes=1:ppn=10:thinnode
 #PBS -l walltime=100:00:00
-#PBS -l mem=100gb
+#PBS -l mem=60gb
 #PBS -m n
 #PBS -r n
 
@@ -24,8 +24,15 @@ module load tools
 module load ngs
 module load samtools/1.12
 
-######################################################################################################################
-for file in $(ls 06_realigned/*.bam|sed -e 's/.nocig.dedup_clipoverlap.minq10_minq10.nocig.realigned.bam//'|sort -u); do
-    samtools depth -aa "$file".nocig.dedup_clipoverlap.minq10_minq10.nocig.realigned.bam | cut -f 3 | gzip > 07_depth/"$file"_depth.gz
+#move to present working dir
+cd $PBS_O_WORKDIR
 
-done
+base=__BASE__
+
+######################################################################################################################
+samtools depth -aa "$file".nocig.dedup_clipoverlap.minq10_minq10.nocig.realigned.bam | cut -f 3 | gzip > 07_depth/"$file"_depth.gz
+
+
+
+#file=Vade_02_EKDL210005548-1a-AK11630-AK16248_HHJKNDSX2_L4
+#samtools depth -aa -@ 4 06_realigned/"$file".nocig.dedup_clipoverlap.minq10_minq10.nocig.realigned.bam | cut -f 3 | gzip > 07_depth/"$file"_depth.gz
