aaf3103a2002dfaeb0246af589e97e8dfc9cc63d
Homère J. Alves Monteiro <hmon@aqua.dtu.dk>
2021-08-09 13:15:48 +0200

    samtools 1.12

--- 00_scripts/05_GATK_indelrealigner.sh
+++ 00_scripts/05_GATK_indelrealigner.sh
@@ -23,7 +23,7 @@ echo This job has allocated $NPROCS nodes
 #ressources
 module load tools
 module load ngs
-module load samtools/1.11
+module load samtools/1.12
 module load parallel/20160822
 module load java/1.8.0
 module load bamutil/1.0.14
@@ -68,3 +68,5 @@ java -jar /services/tools/gatk/3.8-0/GenomeAnalysisTK.jar \
 
 ##
 mv *realigned.bam 06_realigned/
+mv *realigned.bai 06_realigned/
+
--- 00_scripts/06_aDepth.sh
+++ 00_scripts/06_aDepth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -d /home/projects/dp_00007/people/hmon/Shucking
 #PBS -W group_list=dp_00007 -A dp_00007
-#PBS -N file_depth
+#PBS -N bam_depth
 #PBS -e 98_log_files/Covstat/covstat.err
 #PBS -o 98_log_files/Covstat/covstat.out
 #PBS -l nodes=2:ppn=10:thinnode
@@ -22,10 +22,10 @@ echo This job has allocated $NPROCS nodes
 #module
 module load tools
 module load ngs
-module load samtools/1.11
+module load samtools/1.12
 
 ######################################################################################################################
-for file in $(ls bams/*.bam|sed -e 's/.bam//'|sort -u); do
-    samtools depth -aa "$file".bam | cut -f 3 | gzip > 07_depth/"$file"_depth.gz
+for file in $(ls 06_realigned/*.bam|sed -e 's/.nocig.dedup_clipoverlap.minq10_minq10.nocig.realigned.bam//'|sort -u); do
+    samtools depth -aa "$file".nocig.dedup_clipoverlap.minq10_minq10.nocig.realigned.bam | cut -f 3 | gzip > 07_depth/"$file"_depth.gz
 
 done
