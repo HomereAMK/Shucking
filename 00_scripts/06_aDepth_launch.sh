26c62a0443112f99ca543a8db4a404c171828165
Homère J. Alves Monteiro <hmon@aqua.dtu.dk>
2021-11-23 12:42:31 -0500

    l

--- 00_scripts/06_aDepth_launch.sh
+++ 00_scripts/06_aDepth_launch.sh
@@ -14,6 +14,13 @@ base=$(basename "$file")
 	toEval="cat 00_scripts/06_aDepth.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/DEPTH_$base.sh
 done
 
+for file in $(ls USAM_MORL/*.bam |sed -e 's/.nocig.dedup_clipoverlap.minq20_minq20.nocig.realigned.1.5X.bam//g'|sort -u)  #only the nocig retry
+do
+
+base=$(basename "$file")
+
+	toEval="cat 00_scripts/06_aDepth.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/DEPTH_$base.sh
+done
 
 #Submit jobs
 for i in $(ls 00_scripts/DEPTH*sh); do qsub $i; done
