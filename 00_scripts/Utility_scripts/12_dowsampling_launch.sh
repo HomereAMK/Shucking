4b8ec6f48c4e66556de2e2d7ca2207a99a5e5fc4
Homère J. Alves Monteiro <hmon@aqua.dtu.dk>
2021-11-23 12:22:51 -0500

    h

--- 00_scripts/Utility_scripts/12_dowsampling_launch.sh
+++ 00_scripts/Utility_scripts/12_dowsampling_launch.sh
@@ -8,21 +8,21 @@ do
 
 base=$(basename "$file")
 
-	toEval="cat 00_scripts/12_downsamp.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/DOWNSAMP_$base.sh
+	toEval="cat 00_scripts/Utility_scripts/12_downsamp.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/Utility_scripts/DOWNSAMP_$base.sh
 done
 
 
 #Submit jobs
-for i in $(ls 00_scripts/DOWNSAMP*sh); do qsub $i; done
+for i in $(ls 00_scripts/Utility_scripts/DOWNSAMP*sh); do qsub $i; done
 
 for file in $(ls 06_realigned/USAM*.bam|sed -e 's/.bam//g'|sort -u)
 do
 
 base=$(basename "$file")
 
-	toEval="cat 00_scripts/12_downsamp.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/DOWNSAMP_$base.sh
+	toEval="cat 00_scripts/Utility_scripts/12_downsamp.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/Utility_scripts/DOWNSAMP_$base.sh
 done
 
 
 #Submit jobs
-for i in $(ls 00_scripts/DOWNSAMP*sh); do qsub $i; done
+for i in $(ls 00_scripts/Utility_scripts/DOWNSAMP*sh); do qsub $i; done
